d3da121f6b38821c74d43ef83f8675df1b08a36e
renshuai0202 <renshuai0202@gmail.com>
2023-05-01 08:40:04 +0800

    feat: JavaScript的对象是一种散列表

--- Algorithms/查找/查找是否有重复的数字/index.js
+++ Algorithms/查找/查找是否有重复的数字/index.js
@@ -1,3 +1,4 @@
+// 散列表
 function hasRepetitiveNumber(array) {
   const len = array.length;
   let i = 0, arrayTemp = [];
@@ -12,3 +13,18 @@ function hasRepetitiveNumber(array) {
 
 console.log(hasRepetitiveNumber([1, 2, 3, 4, 5, 6]));
 console.log(hasRepetitiveNumber([1, 2, 3, 4, 4, 5]));
+
+function hasRepetitiveNumber2(array) {
+  const len = array.length;
+  let store = {}, i = 0;
+  for(i; i < len; i++) {
+    if(store[array[i]] === undefined) {
+      store[array[i]] = 1;
+    } else {
+      return -1;
+    }
+  }
+}
+
+console.log(hasRepetitiveNumber2([1, 2, 3, 4, 5, 6]));
+console.log(hasRepetitiveNumber2([1, 2, 3, 4, 4, 5]));
